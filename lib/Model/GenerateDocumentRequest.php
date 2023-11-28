15d9e0ebdf925940151edaf24506e6a5ecf89386
Danilo Bispo <danilobispogalvao@gmail.com>
2023-11-28 15:35:25 -0300

    Added additional check in fclose due to warning transformed in exception from PHP 7 to 8.0 + added some return types to remove warnings of deprecation in HTTP 8.0 Tested with versions 7.4.9 and 8.3

--- lib/Model/GenerateDocumentRequest.php
+++ lib/Model/GenerateDocumentRequest.php
@@ -497,7 +497,7 @@ class GenerateDocumentRequest implements ModelInterface, ArrayAccess
      *
      * @return boolean
      */
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return isset($this->container[$offset]);
     }
@@ -509,7 +509,7 @@ class GenerateDocumentRequest implements ModelInterface, ArrayAccess
      *
      * @return mixed
      */
-    public function offsetGet($offset)
+    public function offsetGet($offset): bool
     {
         return isset($this->container[$offset]) ? $this->container[$offset] : null;
     }
@@ -522,7 +522,7 @@ class GenerateDocumentRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         if (is_null($offset)) {
             $this->container[] = $value;
@@ -538,7 +538,7 @@ class GenerateDocumentRequest implements ModelInterface, ArrayAccess
      *
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         unset($this->container[$offset]);
     }
